0f090b31f385b6d71f7dd1ae6c5943b61a73b0ed
mathewo20 <mo237704937@gmail.com>
2025-10-23 13:10:33 +0000

    i finished this introduction with using only print statements

--- mathew_main.py
+++ mathew_main.py
@@ -1,3 +1,13 @@
 print("Hello welcome to scrape quotes ")
 
-print()
+print("This project was created by team: ultra peachy")
+
+
+print("contributing memebers:" )
+print("Eduardo A: load_quotes_from_disk")
+print("Mathew O and Adam D: group_intro()")
+print("Stephanie M: scrape_all_quotes()")
+print("Arham U: get_random code")
+print("Anthony M: save_quotes_to_disk")
+
+print("The purpose of this project is to build a command line tool to scrape and display quotes from web ")
