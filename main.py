9e212802ac57872e067a8fe6a22f8d727c871069
Add879 <adam.playzboi@gmail.com>
2025-10-27 13:11:43 +0000

    ....

--- main.py
+++ main.py
@@ -27,44 +27,14 @@ multi_quote = [
 	},
 ]
 
+def group_introductions():
+	UNDERLINE = '\033[4m'
+	END = '\033[0m'  # Resets all formatting
 
-# SECTION 1: IMPORTS
-# All team members: Add the libraries you need for your function here.
-# import requests
-# import json
-# import random
-# from bs4 import BeautifulSoup
+	print("This project was created by: Adam, TD, Arham, Stephanie, Eduardo, Anthony, Matthew")
+	print(UNDERLINE + "Functions Worked On" + END)
+	print("Adam and Matthew: group_introductions")
 
-
-# ==================================
-# SECTION 2: FUNCTION DEFINITIONS
-# ==================================
-
-# --- Function for Student A ---
-# TODO: Put your scrape_all_quotes function here.
-# This function should scrape all quotes from the website.
-# It should return a list of quote dictionaries.
-
-
-# --- Function for Student B ---
-# TODO: Put your save_quotes_to_disk function here.
-# This function should take the list of quotes and a filename.
-# It should save the quotes to a JSON or CSV file.
-
-
-# --- Function for Student C ---
-# TODO: Put your load_quotes_from_disk function here.
-# This function should take a filename.
-# If the file exists, it returns the list of quotes from the file.
-# If the file does not exist, it returns an empty list [].
-
-
-# --- Function for Student D ---
-# TODO: Put your get_quotes_by_tag function here.
-# This function should take the list of quotes.
-# It asks the user for a tag and prints any matching quotes.
-
-mmm
 # --- Function for Student E ---
 # TODO: Put your get_random_quote function here.
 # This function should take the list of quotes.
