d2b152dad050bacde57769d57c39b1b80e5f6d48
stephaniee3e <158521537+stephaniee3e@users.noreply.github.com>
2025-10-29 13:01:57 +0000

    get_random_quote-stephanie

--- main.py
+++ main.py
@@ -1,51 +1,13 @@
-# ==================================
-#      Python Quote Scraper
-#
-# Team: [Your Team Name]
-# Members: [List of team member names]
-# ==================================
-
-# SECTION 1: IMPORTS
-# All team members: Add the libraries you need for your function here.
-# import requests
-# import json
-# import random
-# from bs4 import BeautifulSoup
-
-
-# ==================================
-# SECTION 2: FUNCTION DEFINITIONS
-# ==================================
-
-# --- Function for Student A ---
-# TODO: Put your scrape_all_quotes function here.
-# This function should scrape all quotes from the website.
-# It should return a list of quote dictionaries.
-
-
-# --- Function for Student B ---
-# TODO: Put your save_quotes_to_disk function here.
-# This function should take the list of quotes and a filename.
-# It should save the quotes to a JSON or CSV file.
-
-
-# --- Function for Student C ---
-# TODO: Put your load_quotes_from_disk function here.
-# This function should take a filename.
-# If the file exists, it returns the list of quotes from the file.
-# If the file does not exist, it returns an empty list [].
-
-
-# --- Function for Student D ---
-# TODO: Put your get_quotes_by_tag function here.
-# This function should take the list of quotes.
-# It asks the user for a tag and prints any matching quotes.
-
-
 # --- Function for Student E ---
 # TODO: Put your get_random_quote function here.
 # This function should take the list of quotes.
 # It picks one random quote and prints it.
+def get_random_quote(quotes):
+    if not quotes:
+         print("No quotes available.")
+        return
+    random_quote = random.choice(quotes)
+    print(random_quote)
 
 
 
