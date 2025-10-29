8fe27c14761001af328a2966e35e6d389392430a
uniquestar231 <Uniquestar231@gmail.com>
2025-10-29 13:05:44 +0000

    dd

--- main.py
+++ main.py
@@ -1,51 +1,32 @@
-# ==================================
-#      Python Quote Scraper
-#
-# Team: [Your Team Name]
-# Members: [List of team member names]
-# ==================================
+from bs4 import BeautifulSoup
+import requests 
+def scrape_all_quotes():
+url = 'https://quotes.toscrape.com'
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
 
-# SECTION 1: IMPORTS
-# All team members: Add the libraries you need for your function here.
-# import requests
-# import json
-# import random
-# from bs4 import BeautifulSoup
+quotes = []
 
+while True:
+    # Find all quote blocks on the page
+   for quote in soup.select('div.quote'):
+        text = quote.select_one('span.text').get_text(strip=True)
+        author = quote.select_one('small.author').get_text(strip=True)
+        tags = [tag.get_text(strip=True) for tag in quote.select('div.tags a.tag')]
+        quotes.append({'text': text, 'author': author, 'tags': tags})
+    
+    # Find the link to the next page, if available
+    next_btn = soup.select_one('li.next a')
+    if next_btn:
+        next_url = url + next_btn['href']
+        page = requests.get(next_url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+    else:
+        break
 
-# ==================================
-# SECTION 2: FUNCTION DEFINITIONS
-# ==================================
-
-# --- Function for Student A ---
-# TODO: Put your scrape_all_quotes function here.
-# This function should scrape all quotes from the website.
-# It should return a list of quote dictionaries.
-
-
-# --- Function for Student B ---
-# TODO: Put your save_quotes_to_disk function here.
-# This function should take the list of quotes and a filename.
-# It should save the quotes to a JSON or CSV file.
-
-
-# --- Function for Student C ---
-# TODO: Put your load_quotes_from_disk function here.
-# This function should take a filename.
-# If the file exists, it returns the list of quotes from the file.
-# If the file does not exist, it returns an empty list [].
-
-
-# --- Function for Student D ---
-# TODO: Put your get_quotes_by_tag function here.
-# This function should take the list of quotes.
-# It asks the user for a tag and prints any matching quotes.
-
-
-# --- Function for Student E ---
-# TODO: Put your get_random_quote function here.
-# This function should take the list of quotes.
-# It picks one random quote and prints it.
+for q in quotes:
+    print(q)
+ 
 
 
 
@@ -56,12 +37,3 @@
 
 
 
-
-
-# ==================================
-# SECTION 3: MAIN PROGRAM
-# ==================================
-
-
-# Team Lead/Integrator: Write the main logic here that calls the functions.
-if __name__ == "__main__":
