500ba6440272aea70b0137fb90b09666722536ca
Nycwp123 <161650040+Nycwp123@users.noreply.github.com>
2025-10-29 05:26:39 +0000

    Save protion

--- main.py
+++ main.py
@@ -51,6 +51,93 @@ multi_quote = [
 # This function should take the list of quotes and a filename.
 # It should save the quotes to a JSON or CSV file.
 
+from bs4 import BeautifulSoup
+import requests 
+
+url = 'https://quotes.toscrape.com'
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
+
+quotes = []
+
+while True:
+    # Find all quote blocks on the page
+    for quote in soup.select('div.quote'):
+        text = quote.select_one('span.text').get_text(strip=True)
+        author = quote.select_one('small.author').get_text(strip=True)
+        tags = [tag.get_text(strip=True) for tag in quote.select('div.tags a.tag')]
+        quotes.append({'text': text, 'author': author, 'tags': tags})
+    
+    # Find the link to the next page, if available
+    next_btn = soup.select_one('li.next a')
+    if next_btn:
+        next_url = url + next_btn['href']
+        page = requests.get(next_url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+    else:
+        break
+
+
+
+
+import json
+
+person_JSON = {'text': text, 'author': author, 'tags': tags}
+person_JSON = json.dumps(author)
+
+with open('person.json', 'w') as json_file:
+	json_file.write(person_JSON)
+	print('Saved succesfully!')
+
+
+#import json
+#date_str = input("Please put in the date:")
+
+#def save_quotes_to_disk(quotes, date_str):
+    #filename = f"save_quotes_to_disk{date_str}.json"
+    #with open(filename, "w", encoding="utf-8") as f:
+        #json.dump(quotes, f, indent=4, ensure_ascii=False)
+    #print(f"Quotes saved to {filename}")
+
+
+
+
+
+
+
+
+#from bs4 import JSON
+
+#import request 
+
+#url = https://qprintuotes.toscrape.com/
+
+#file_name = input("Please put in the date:")
+
+#def save_quotes_to_disk_json(list , file_name):
+	#with open('save_quotes_to_disk_json', file_name) as f:
+		#json.dump(save_quotes_to_disk_json, f)
+#save_quotes_to_disk_json(list , file_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # --- Function for Student C ---
 # TODO: Put your load_quotes_from_disk function here.
@@ -87,4 +174,4 @@ multi_quote = [
 
 
 # Team Lead/Integrator: Write the main logic here that calls the functions.
-if __name__ == "__main__":
+
