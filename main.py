3b7e4a9902478410f90ad24ef5c6a19165642a82
Add879 <adam.playzboi@gmail.com>
2025-10-29 12:53:04 +0000

    ...

--- main.py
+++ main.py
@@ -27,14 +27,20 @@ multi_quote = [
 	},
 ]
 
-def group_introductions():
+def group_introductions_adam():
 	UNDERLINE = '\033[4m'
 	END = '\033[0m'  
 
 	print("This project was created by: Adam, TD, Arham, Stephanie, Eduardo, Anthony, and Matthew")
 	print("-----------------------------")
 	print(UNDERLINE + "Functions Worked On" + END)
-	print("Adam and Matthew: group_introductions")
+	print("TD: Captain")
+	print("Adam and Matthew: group_introductions. They are displaying a text that introduces all group members, what they worked on, and the purpose of this project")
+	print("Arham: scrape_all_quotes. He is scraping all quotes from every page of the website.")
+	print("Stephanie: get_random_quote(quotes_list). She is displaying a random quote to the user using the website")
+	print("Anthony: save_quotes_to_disk(quotes_list, date_str). He is saving the scrape to the local file.")
+	print("Eduardo: load_quotes_from_disk(filename). He is loading all the quotes from the previously saved file which helps avoid the need to scrape again.")
+	
 
 # --- Function for Student E ---
 # TODO: Put your get_random_quote function here.
