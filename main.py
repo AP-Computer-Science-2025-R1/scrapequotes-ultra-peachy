8f4edf92efd0db7d2023b12cce2c14b18dc8fe90
uniquestar231 <Uniquestar231@gmail.com>
2025-10-30 08:42:15 +0000

    edited code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
-import requests 
-def scrape_all_quotes():
+import requests
+
 url = 'https://quotes.toscrape.com'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
@@ -8,14 +8,12 @@ soup = BeautifulSoup(page.text, 'html.parser')
 quotes = []
 
 while True:
-    # Find all quote blocks on the page
-   for quote in soup.select('div.quote'):
+    for quote in soup.select('div.quote'):
         text = quote.select_one('span.text').get_text(strip=True)
         author = quote.select_one('small.author').get_text(strip=True)
         tags = [tag.get_text(strip=True) for tag in quote.select('div.tags a.tag')]
         quotes.append({'text': text, 'author': author, 'tags': tags})
     
-    # Find the link to the next page, if available
     next_btn = soup.select_one('li.next a')
     if next_btn:
         next_url = url + next_btn['href']
@@ -26,14 +24,3 @@ while True:
 
 for q in quotes:
     print(q)
- 
-
-
-
-
-
-
-
-
-
-
